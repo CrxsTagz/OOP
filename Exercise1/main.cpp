33f36191fc10c16b79fccf43596fba1c5fc4c375
CrxsTagz <mynameiscris000@gmail.com>
2020-10-09 14:57:47 -0400

    Adding main and shape to the folder

---
+++ Exercise1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <shape.hpp>
+
+int main (){
+
+	Rectangle rect;
+	Triangle tri;
+	rect.shape_data (10,25;
+	tri.shape_data (15,45);
+	cout <<"The area of the rectangle is: " << rect.area() << endl;
+	cout << "The area of the triangle is: " << trig.area() << endl;
+	return 0;
+}
