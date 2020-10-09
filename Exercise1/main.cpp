1edeba2e2d949df360facc4f306897e6b410832a
CrxsTagz <mynameiscris000@gmail.com>
2020-10-09 15:45:08 -0400

    fixed a letter error in main.cpp

--- Exercise1/main.cpp
+++ Exercise1/main.cpp
@@ -7,8 +7,8 @@ int main ()
 	Rectangle rect;
 	Triangle tri;
 	rect.shape_data (10,25);
-	trig.shape_data (15,45);
+	tri.shape_data (15,45);
 	cout <<"The area of the rectangle is: " << rect.area() << endl;
-	cout << "The area of the triangle is: " << trig.area() << endl;
+	cout << "The area of the triangle is: " << tri.area() << endl;
 	return 0;
 }
